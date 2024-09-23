2c229471fbe3a1876302c62924a3d2565e40820f
Zlynt <ivan.teixeira@zlyntlab.com>
2024-09-23 08:55:20 +0100

    Implemented Argon2

--- src/main/java/com/zlyntlab/ondabi/crypto/Argon2.java
+++ src/main/java/com/zlyntlab/ondabi/crypto/Argon2.java
@@ -2,6 +2,7 @@ package com.zlyntlab.ondabi.crypto;
 
 import org.bouncycastle.crypto.generators.Argon2BytesGenerator;
 import org.bouncycastle.crypto.params.Argon2Parameters;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
@@ -15,42 +16,20 @@ public class Argon2 {
     private static final int memorySize     = 65536;
     private static final int pararelism  = 2;
 
+    private Argon2PasswordEncoder encoder;
+
     public Argon2(){
         super();
-    }
-
-    private byte[] genSalt() {
-        byte[] salt = new byte[saltLength];
-        new SecureRandom().nextBytes(salt);
-
-        return salt;
+        this.encoder = new Argon2PasswordEncoder(
+                saltLength, hashLength, pararelism, memorySize, interactions
+        );
     }
 
     public String hash(String information) {
-        byte[] salt = this.genSalt();
-
-        Argon2Parameters.Builder builder = new Argon2Parameters.Builder(Argon2Parameters.ARGON2_id)
-                .withSalt(salt)
-                .withParallelism(pararelism)
-                .withMemoryAsKB(memorySize)
-                .withIterations(interactions);
-
-        Argon2BytesGenerator generate = new Argon2BytesGenerator();
-        generate.init(builder.build());
-        byte[] result = new byte[hashLength];
-        generate.generateBytes(information.getBytes(StandardCharsets.UTF_8), result, 0, result.length);
-
-        String hash = "";
-        for(byte i : result){
-            hash += String.format("%02X", i);
-        }
-
-        return hash;
+        return this.encoder.encode(information);
     }
 
     public Boolean verify(String information, String hash) {
-        String hashedInformation = this.hash(information);
-
-        return hashedInformation.equals(hash);
+        return this.encoder.matches(information, hash);
     }
 }
